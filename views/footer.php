f96f7bea217e9780d582204b455cbfd4b670394f
Januki-Kaluarachchi <madse009nibm@gmail.com>
2026-09-02 07:46:54 +0530

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
 <footer class="py-4 mt-5" style="background-color: #0a0a0a; border-top: 2px solid var(--gold); color: #aaa;">
     <div class="container text-center">
-        <p class="mb-0">&copy; 2026 CampusPulse System. Powered by Oracle PL/SQL & MongoDB.</p>
+        <p class="mb-0 fw-semibold" style="color: #d1d1d1;">&copy; 2026 CampusPulse System. Created by Januu</p>
     </div>
 </footer>
 
